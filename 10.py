58b696d272d61a8e3eb6ca0afb3b94b959574ec1
Poorvika <poorvikaa08@gmail.com>
2026-07-26 23:42:41 +0530

    update

--- 10.py
+++ 10.py
@@ -1,11 +1,9 @@
-
-# Write a program to develop Principal Component Analysis (PCA) algorithm.
-
-#PCA with sklearn
+# Write a program to develop Principal Component Analysis (PCA) and Linear Discriminant Analysis (LDA) algorithm.
 
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
-from sklearn.decomposition import PCA as PCA
+from sklearn.decomposition import PCA 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 # Load the Iris dataset
 X = load_iris().data
@@ -13,17 +11,34 @@ y = load_iris().target
 
 # Perform PCA using sklearn
 pca = PCA(n_components=2)
-X_projected = pca.fit_transform(X)
+pca_projected = pca.fit_transform(X)
 
 print("Shape of Data:", X.shape)
-print("Shape of transformed Data:", X_projected.shape)
+print("Shape of transformed Data:", pca_projected.shape)
 
 # Plot the results
-pc1 = X_projected[:, 0]
-pc2 = X_projected[:, 1]
+pc1 = pca_projected[:, 0]
+pc2 = pca_projected[:, 1]
 
 plt.scatter(pc1, pc2, c=y, cmap="jet")
 plt.xlabel("Principal Component 1")
 plt.ylabel("Principal Component 2")
 plt.title("PCA of Iris Dataset (sklearn Implementation)")
+plt.show()
+
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+lda_projected = lda.fit_transform(X, y)
+
+print("Shape of Data:", X.shape)
+print("Shape of transformed Data:", lda_projected.shape)
+
+# Plot the results
+ld1 = lda_projected[:, 0]
+ld2 = lda_projected[:, 1]
+
+plt.scatter(ld1, ld2, c=y, cmap="jet")
+plt.xlabel("Linear Discriminant 1")
+plt.ylabel("Linear Discriminant 2")
+plt.title("LDA of Iris Dataset (sklearn Implementation)")
 plt.show()
--- 11.py
+++ 11.py
@@ -1,26 +1,85 @@
-# Write a program to develop Linear Discriminant Analysis (LDA) algorithm.
+# Write a Program to develop simple single layer perceptron to implement AND, OR Boolean functions. 
 
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import numpy as np
 
-# Load the Iris dataset
-X = load_iris().data
-y = load_iris().target
+# Sigmoid activation function
+# Converts any value into a number between 0 and 1
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-# Perform LDA using sklearn
-lda = LinearDiscriminantAnalysis(n_components=2)
-X_projected = lda.fit_transform(X, y)
+# Function to train the perceptron
+def train_perceptron(X, y):
 
-print("Shape of Data:", X.shape)
-print("Shape of transformed Data:", X_projected.shape)
+    w = np.random.rand(2)
+    b = np.random.rand()
 
-# Plot the results
-ld1 = X_projected[:, 0]
-ld2 = X_projected[:, 1]
+    for _ in range(1000):
+        for i in range(len(X)):
 
-plt.scatter(ld1, ld2, c=y, cmap="jet")
-plt.xlabel("Linear Discriminant 1")
-plt.ylabel("Linear Discriminant 2")
-plt.title("LDA of Iris Dataset (sklearn Implementation)")
-plt.show()
+            # Output = sigmoid(X.W + b)
+            out = sigmoid(np.dot(X[i], w) + b)
+
+            # prediction error
+            error = y[i] - out
+
+            # New Weight = Old Weight + Learning Rate × Error × Input
+            w += 0.1 * error * X[i]
+
+            # Update bias
+            b += 0.1 * error
+
+    # Return learned weights and bias
+    return w, b
+
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
+
+
+y_and = np.array([0, 0, 0, 1])
+y_or = np.array([0, 1, 1, 1])
+
+w1, b1 = train_perceptron(X, y_and)
+w2, b2 = train_perceptron(X, y_or)
+
+print("AND Gate")
+
+for x in X:
+    output = sigmoid(np.dot(x, w1) + b1)   # Predict output
+    print(x, "->", round(output)) # Round to 0 or 1
+
+
+print("\nOR Gate")
+
+for x in X:
+    output = sigmoid(np.dot(x, w2) + b2)
+    print(x, "->", round(output))
+    
+    
+    
+    
+    
+    
+# class Perceptron:
+#     def __init__(self):
+#         self.weights = np.random.rand(2) # 2=input size
+#         self.bias = np.random.rand()
+
+#     def predict(self, x):
+#         return sigmoid(np.dot(x, self.weights) + self.bias)
+
+#     def train(self, X, y, epochs=1000, lr=0.1):
+#         for _ in range(epochs):
+#             for x, target in zip(X, y):
+#                 error = target - self.predict(x)
+#                 self.weights += lr * error * x
+#                 self.bias += lr * error
+
+# X = np.array([[0,0],[0,1],[1,0],[1,1]])
+# Y = {"AND":[0,0,0,1], "OR":[0,1,1,1]}
+
+# for gate, y in Y.items():
+#     p = Perceptron()
+#     p.train(X, np.array(y))
+
+#     print(f"\n{gate} Gate")
+#     for x in X:
+#         print(x, "->", round(float(p.predict(x))))
